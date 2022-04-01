5736f85214d25b3fcda5b92cab9b0aef8bb4ed81
alme7airbi93 <alme7airbi93@live.com>
2022-04-02 02:31:08 +0400

    Fixing fonts

--- src/nav-bar/Header.js
+++ src/nav-bar/Header.js
@@ -9,9 +9,9 @@ export const Header = () => {
                 <Container className="header-nav-container" fluid>
                     <Navbar.Brand><img className="header-icon" alt="This is company logo" src={logo}/></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                    <Container>
+                    <Container >
                         <Navbar.Collapse id="responsive-navbar-nav"  className="header-container justify-content-end">
-                            <Nav>
+                            <Nav className="gap-5">
                                 <Nav.Link><span className="nav-text">HOME</span></Nav.Link>
                                 <Nav.Link><span className="nav-text">ABOUT US</span></Nav.Link>
                                 <Nav.Link><span className="nav-text">OUR SERVICES</span></Nav.Link>
