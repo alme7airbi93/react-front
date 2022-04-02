a733b79d2df48336a02c1a99ebd90550e942fc23
alme7airbi93 <alme7airbi93@live.com>
2022-04-02 05:13:14 +0400

    Adding Text to Intro

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -3,13 +3,32 @@ import "./Intro.css"
 import backGround from "../assets/intro/bg-img.png";
 import Logo from "../assets/intro/logo_1.png"
 
-export const Intro = () =>{
-    return(
+export const Intro = () => {
+    return (
         <Container fluid={true} className="container-intro ">
             <div className="background-image">
                 <img className="img-fluid w-100" src={backGround} alt={""}/>
                 <div className="logo-image">
-                    <img className="img-fluid " src={Logo} alt={""}/>
+                    <img className="img-fluid logo" src={Logo} alt={""}/>
+                </div>
+                <div className="container-text ">
+                    <ul className="container-ul">
+                        <li>
+                            <span className="container-text-first ">
+                                WE BRING TECHNOLOGY
+                            </span>
+                        </li>
+                        <li>
+                            <span className="container-text-second p-3 mt-2">
+                            TO YOUR
+                            </span>
+                        </li>
+                        <li>
+                            <span className="container-text-third ">
+                            FINGERTIPS
+                            </span>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </Container>
