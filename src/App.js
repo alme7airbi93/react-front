a36c78c002464aa7c87c5aa0584b761af5603963
Green Dream <101880509+greendream0306@users.noreply.github.com>
2022-04-03 18:10:31 +0900

    frontend update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import {Container} from "react-bootstrap";
 import {Intro} from "./intro/Intro";
 import {Services} from "./services/Services";
 import {VideoContent} from "./video-content/VideoContent";
-
+import { AboutUs } from './aboutUs/AboutUs';
+import { Serve } from './serve/Serve';
+import { Form } from './contact/Form';
+import { Footer } from './footer/Footer';
+ 
 function App() {
   return (
     <Container fluid={true} className="App" >
@@ -12,6 +16,10 @@ function App() {
             <Intro/>
             <Services/>
             <VideoContent/>
+            <AboutUs />
+            <Serve />
+            <Form />
+            <Footer />
     </Container>
   );
 }
